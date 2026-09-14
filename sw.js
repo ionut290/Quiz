527c3332897cc2ffd2724906518307283e45ce4e
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-09-14 11:07:58 +0200

    Rotate unseen questions between quiz rounds

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='quizmix-v6';
+const CACHE='quizmix-v7';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
