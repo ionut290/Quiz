e18d5ca702b929188e8b175a04b9aa2ecea01f1f
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-09-15 07:33:51 +0200

    Forza aggiornamento PWA Varga Quiz

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='quizmix-v10';
+const CACHE='quizmix-v11';
 const APP_SHELL=['./','./index.html','./patente-b.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
